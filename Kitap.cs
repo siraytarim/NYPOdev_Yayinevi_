8b64db6dcf513b492af4811058488ee168220203
Sıray Tarım <99121035+siraytarim@users.noreply.github.com>
2023-05-20 17:29:42 +0300

    Add files via upload
    Classes

---
+++ Kitap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta8_Odev
+{
+    public class Kitap
+    {
+        public string ad;
+        yayınevi yE;
+        public Kitap(string AD)
+        {
+            this.ad = AD;
+        }
+        public Kitap(string ad, yayınevi yE)
+        {
+            this.ad = ad;
+            this.yE = yE;
+        }
+
+        public string Ad { get { return ad; } set { ad = value; } }
+           }
+}
---
+++ yayınevi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta8_Odev
+{
+    public class yayınevi
+    {
+        public string ad;
+
+        List<Kitap> ykitaplar = new List<Kitap>();
+        List<Editor> editors = new List<Editor>();  
+
+        public yayınevi(string ad)
+        {
+            this.ad = ad;
+            this.editors = new List<Editor>();
+            this.ykitaplar = new List<Kitap>(); 
+        }
+        public string Ad() { return this.ad; }
+    }
+}
